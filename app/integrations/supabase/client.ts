af336d6aa0af89a32b3374fdcf113d9314bec5d6
natively.dev <api@natively.dev>
2026-02-26 14:45:41 +0000

    fix

--- app/integrations/supabase/client.ts
+++ app/integrations/supabase/client.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Database } from './types';
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://orzzjwgteknzqmymampw.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yenpqd2d0ZWtuenFteW1hbXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzI5OTYsImV4cCI6MjA4NzU0ODk5Nn0.BP32iXVO98dvYE_5UFIwxR1PiFiFkimnjAkNZO9r0yw";
+const SUPABASE_URL = "https://blpwphjjpssmwekugain.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscHdwaGpqcHNzbXdla3VnYWluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMDUwMzgsImV4cCI6MjA4NzY4MTAzOH0.4hlCE7u3WVZeqOadK-jELtnf5H51NyJ_ucuiMXq-0PQ";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
